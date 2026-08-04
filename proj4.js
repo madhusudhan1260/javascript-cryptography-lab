c6e94895934f6c952aaabae8acad304afbca92a1
Madhusudhan ramshetty <madhusudhan@Mac.lan>
2026-08-04 22:21:03 +0530

    Added JavaScript Assignment 4

---
+++ proj4.js
@@ -0,0 +1,31 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter first number: ", (num1) => {
+    rl.question("Enter second number: ", (num2) => {
+        rl.question("Enter third number: ", (num3) => {
+
+            num1 = parseFloat(num1);
+            num2 = parseFloat(num2);
+            num3 = parseFloat(num3);
+
+            let largest;
+
+            if (num1 >= num2 && num1 >= num3) {
+                largest = num1;
+            } else if (num2 >= num1 && num2 >= num3) {
+                largest = num2;
+            } else {
+                largest = num3;
+            }
+
+            console.log("Largest number =", largest);
+
+            rl.close();
+        });
+    });
+});
