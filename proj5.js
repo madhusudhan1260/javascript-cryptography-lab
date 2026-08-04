80bb2b730d3c60849636ba0696746521d6500051
Madhusudhan ramshetty <madhusudhan@Mac.lan>
2026-08-04 22:23:01 +0530

    Added JavaScript Assignment 5

---
+++ proj5.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter a string: ", (str) => {
+    let reverse = str.split("").reverse().join("");
+
+    console.log("Reversed String:", reverse);
+
+    rl.close();
+});
